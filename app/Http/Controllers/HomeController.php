e895f7c6b42c38330c4574d07ab5dc2cfed0d51f
Mohammed Che <mocherbatji@gmail.com>
2023-11-27 12:02:32 +0000

    Added Home page and AuthorizeRole

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $user = Auth::user();
+
+        $home = 'home';
+
+        if($user->hasRole('admin')){
+            $home = 'admin.home';
+        }
+        else if($user->hasRole('user')){
+            $home = 'user.home';
+        }
+        
+        return view($home);
+        // return redirect()->route($home);
+
+    }
+}
